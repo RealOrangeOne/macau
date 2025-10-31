1af2ab5f3d14a15fea5df73d491e2cfcf339ee36
Jake Howard <git@theorangeone.net>
2025-10-31 12:11:01 +0000

    Stick admin in a namespace
    This prevents conflicting with redirects

--- macau/urls.py
+++ macau/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import admin
 
 urlpatterns = [
-    path("admin/", admin.admin_site.urls),
-    path("-/health/", include("health_check.urls")),
     path("", include("macau.redirects.urls")),
+    path("-/admin/", admin.admin_site.urls),
+    path("-/health/", include("health_check.urls")),
 ]
