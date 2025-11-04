bbab0887f986f3c12086d53beacba1e542dc1a3e
Jake Howard <git@theorangeone.net>
2025-11-04 21:43:07 +0000

    Get correct IP from clients

--- macau/wsgi.py
+++ macau/wsgi.py
@@ -1,7 +1,8 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from granian.utils.proxies import wrap_wsgi_with_proxy_headers
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "macau.settings")
 
-application = get_wsgi_application()
+application = wrap_wsgi_with_proxy_headers(get_wsgi_application(), trusted_hosts="*")
